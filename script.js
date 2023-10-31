298e9f7a3a74607e138734dff1de8910ba96c041
aloofist <98794169+aloofist@users.noreply.github.com>
2023-10-31 21:34:07 +0800

    if statements done

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const input = document.querySelector("input")
 const inputText = document.querySelector("input[type='text']")
 const email = document.getElementById("email")
+const password = document.getElementById("password")
 
 const tosCheck = document.querySelector("input[type='checkbox']")
-const createButton = document.querySelector(".card button")
+const createAcc = document.querySelector(".card button")
 
 const link = document.querySelector("#link")
 
@@ -11,26 +12,29 @@ const link = document.querySelector("#link")
 
 function checkInputs() {
     if (inputText.value === "") {
-        alert("You didn't fill in everything!")
+        alert("You didn't fill in anything!")
     } else if (!email.value.includes("@")) {
         alert("Invaild email address")
-        console.log("Invalid email address")
+    } else if (password.value === "") {
+        alert("Password is required")
+    } else if (password.value.length < 8) {
+        alert("Password needs to be at least 8 characters")
+    } else if (!tosCheck.checked) {
+        alert("You haven't agreed to Terms of Service ad Privacy Policy")
     } else {
         link.href = "signed-up.html"
         createButton.style.opacity = "1"
     }
 }
 
+// function check() {
+//     if (inputText.value !== "" && email.value.includes("@") && tosCheck.checked) {
+//         createButton.style.opacity = "1"
+//         console.log("aa")
+//     } else {
+//         createButton.style.opacity = "0.5"
+//     }
+// }
 
-function check() {
-    if (inputText.value === "") {
-        createButton.style.opacity = "0.5"
-    } else if (!email.value.includes("@")) {
-        createButton.style.opacity = "0.5"
-    } else {
-        createButton.style.opacity = "1"
-    }
-}
-
-createButton.addEventListener("click", checkInputs)
-input.addEventListener("input", )
+createAcc.addEventListener("click", checkInputs)
+// input.addEventListener("input", check)
