7b641c6e35165de55f9755765a063a64975ddae3
aloofist <98794169+aloofist@users.noreply.github.com>
2023-11-17 19:52:28 +0800

    sign out button

--- signed-up.js
+++ signed-up.js
@@ -1,4 +1,9 @@
 const userEl = document.getElementById("user-el")
 const userName = localStorage.getItem("user")
+const signOutBtn = document.getElementById("sign-out")
 
 userEl.innerText = userName
+
+signOutBtn.addEventListener("click", () => {
+    localStorage.removeItem("user")
+});
